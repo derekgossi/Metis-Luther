55348158c35782c91c52d795cd60af3852b5ac29
Derek Gossi <gossiderek@gmail.com>
2015-01-23 10:15:31 -0500

    log-log regression model of num followers

--- test_project.py
+++ test_project.py
@@ -228,92 +228,131 @@ def twi_factors_from_search_response(json_data):
 
 ### GET TWITTER DATA OF ACTRESS OR ACTOR
 
-import oauth2 as oauth
-import json
+# import oauth2 as oauth
+# import json
 
-# Oauth2 for Twitter API data
-CONSUMER_KEY = "AeeP8XqdVn4JKkFe1taObuLo7"
-CONSUMER_SECRET = "BAzTezf78LneLG0zVXVaIeG6bXOaCnJ12ennEXZVVNK1plCiwc"
-ACCESS_KEY = "2815366039-1HLdZHBjOf3A2ZStoi2GJPZx8OsobTU5FtAQzON"
-ACCESS_SECRET = "coXCsBzu7T38rU3RQYh9BLGQkQLHXtMqcal1rNYHkqk6e"
+# # Oauth2 for Twitter API data
+# CONSUMER_KEY = "AeeP8XqdVn4JKkFe1taObuLo7"
+# CONSUMER_SECRET = "BAzTezf78LneLG0zVXVaIeG6bXOaCnJ12ennEXZVVNK1plCiwc"
+# ACCESS_KEY = "2815366039-1HLdZHBjOf3A2ZStoi2GJPZx8OsobTU5FtAQzON"
+# ACCESS_SECRET = "coXCsBzu7T38rU3RQYh9BLGQkQLHXtMqcal1rNYHkqk6e"
 
-consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
-client = oauth.Client(consumer, access_token)
+# consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+# access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+# client = oauth.Client(consumer, access_token)
 
-# Load pickledata for data frame
-with open('current_movies_df.p','rb') as picklefile:
+# # Load pickledata for data frame
+# with open('current_movies_df.p','rb') as picklefile:
+#  	actress_data = pickle.load(picklefile)
+
+# # Reset the index to integers
+# actress_data.index = range(len(actress_data))
+
+# # Iterate through names in our data frame, query twitter for follower count, tweets, 
+# # 	and date twitter account started
+
+# # Load current twitter data
+# with open('twitter_data.p','rb') as picklefile:
+#  	list_length, names, followers_all, num_tweets_all, years_on_twi_all = pickle.load(picklefile)
+
+# # Initialize twitter data if needed
+# # list_length = 0
+# # names = []
+# # followers_all = []
+# # num_tweets_all = []
+# # years_on_twi_all = []
+
+# row_iterator = actress_data.iterrows()
+# last = row_iterator.next()
+# row_iterator = actress_data.iterrows()
+
+# # We need to keep track of the queries we have already made bc of rate limiting
+# for i in range(list_length):
+# 	last = row_iterator.next()
+
+# # Make queries to twitter API
+# counter = 0
+# for name, row in row_iterator:
+# 	counter += 1
+
+# 	twi_url_request = get_twi_search_url_from_name(str(row['Names']))
+# 	twi_response, twi_data = client.request(twi_url_request)
+
+# 	# Check rate limit
+# 	queries_left = int(twi_response['x-rate-limit-remaining'])
+# 	if queries_left > 0:
+# 		followers, num_tweets, years_on_twi = twi_factors_from_search_response(twi_data)
+# 		followers_all.append(followers)
+# 		num_tweets_all.append(num_tweets)
+# 		years_on_twi_all.append(years_on_twi)
+# 		names.append(str(row['Names']))
+# 	else: 
+# 		# Write what we have to the file
+# 		with open('twitter_data.p','wb') as picklefile:
+# 			# What is the current length of the lists
+# 			list_length = len(followers_all)
+# 			print (list_length, names, followers_all, num_tweets_all, years_on_twi_all)
+#  			pickle.dump((list_length, names, followers_all, num_tweets_all, years_on_twi_all), picklefile)
+
+#  		break
+
+# 	print str(counter)
+
+# 	# Next row
+# 	last = row
+
+# # Write to file
+# with open('twitter_data.p','wb') as picklefile:
+# 	# What is the current length of the lists
+# 	list_length = len(followers_all)
+# 	print (list_length, names, followers_all, num_tweets_all, years_on_twi_all)
+#  	pickle.dump((list_length, names, followers_all, num_tweets_all, years_on_twi_all), picklefile)
+
+# Open twitter data file and append to dataframe
+# with open('current_movies_df.p','rb') as picklefile:
+#  	actress_data = pickle.load(picklefile)
+
+# with open('twitter_data.p','rb') as picklefile:
+# 	list_length, names, followers_all, num_tweets_all, years_on_twi_all = pickle.load(picklefile)
+
+# actress_data['NumFollowers'] = followers_all
+# actress_data['NumTweets'] = num_tweets_all
+# actress_data['YrsOnTwitter'] = years_on_twi_all
+# actress_data['NumTweetsPerYr'] = actress_data['NumTweets'] / actress_data['YrsOnTwitter']
+
+# # Write to file
+# with open('df_w_twitter.p','wb') as picklefile:
+#  	pickle.dump(actress_data, picklefile)
+
+### REGRESSION MODEL
+import statsmodels.formula.api as sm
+import math
+
+with open('df_w_twitter.p','rb') as picklefile:
  	actress_data = pickle.load(picklefile)
 
 # Reset the index to integers
 actress_data.index = range(len(actress_data))
 
-# Iterate through names in our data frame, query twitter for follower count, tweets, 
-# 	and date twitter account started
+# Log transform dependent variable
+actress_data['ln_NumFollowers'] = np.log(actress_data['NumFollowers'])
+actress_data['ln_NumTweetsPerYr'] = np.log(actress_data['NumTweetsPerYr'])
+actress_data['ln_AvgGross'] = np.log(actress_data['AvgGross'])
+actress_data['ln_Age'] = np.log(actress_data['Age'])
 
-# Load current twitter data
-with open('twitter_data.p','rb') as picklefile:
- 	list_length, names, followers_all, num_tweets_all, years_on_twi_all = pickle.load(picklefile)
+# Remove NaN and outliers
+actress_data_subset = actress_data.dropna()[actress_data['NumFollowers'] <= 1000000]
 
-# Initialize twitter data if needed
-# list_length = 0
-# names = []
-# followers_all = []
-# num_tweets_all = []
-# years_on_twi_all = []
+X = actress_data_subset[['ln_NumTweetsPerYr', 'ln_AvgGross', 'ln_Age']]
+Y = actress_data_subset['ln_NumFollowers']
 
-row_iterator = actress_data.iterrows()
-last = row_iterator.next()
-row_iterator = actress_data.iterrows()
+linmodel = sm.OLS(Y,X).fit()
 
-# We need to keep track of the queries we have already made bc of rate limiting
-for i in range(list_length):
-	last = row_iterator.next()
+print linmodel.summary()
 
-# Make queries to twitter API
-counter = 0
-for name, row in row_iterator:
-	counter += 1
-
-	twi_url_request = get_twi_search_url_from_name(str(row['Names']))
-	twi_response, twi_data = client.request(twi_url_request)
-
-	# Check rate limit
-	queries_left = int(twi_response['x-rate-limit-remaining'])
-	if queries_left > 0:
-		followers, num_tweets, years_on_twi = twi_factors_from_search_response(twi_data)
-		followers_all.append(followers)
-		num_tweets_all.append(num_tweets)
-		years_on_twi_all.append(years_on_twi)
-		names.append(str(row['Names']))
-	else: 
-		# Write what we have to the file
-		with open('twitter_data.p','wb') as picklefile:
-			# What is the current length of the lists
-			list_length = len(followers_all)
-			print (list_length, names, followers_all, num_tweets_all, years_on_twi_all)
- 			pickle.dump((list_length, names, followers_all, num_tweets_all, years_on_twi_all), picklefile)
-
- 		break
-
-	print str(counter)
-
-	# Next row
-	last = row
-
-# Write to file
-with open('twitter_data.p','wb') as picklefile:
-	# What is the current length of the lists
-	list_length = len(followers_all)
-	print (list_length, names, followers_all, num_tweets_all, years_on_twi_all)
- 	pickle.dump((list_length, names, followers_all, num_tweets_all, years_on_twi_all), picklefile)
-
-
-### GET ADDITIONAL DATA ?!?!?!
-
-
-
-
+plt.plot(Y, linmodel.predict(X), 'go')
+plt.plot(Y, Y, 'm--', label = 'Ideal Prediction')
+plt.show()
 
 
 
